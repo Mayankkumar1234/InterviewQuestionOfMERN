ab904853fef5153bd40b43fed4d7746516db49f2
Mayankkumar1234 <mk1070355@gmail.com>
2025-05-29 23:27:01 +0530

    NodeJs

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -4,4 +4,68 @@
 
 
  
-//
+// //# Functions in Js :-
+
+// function abcd(){
+
+//   <!-- - Function Statement -->
+
+// }
+
+// function(){
+
+//   <!-- Anonymous function - Function without name -->
+
+// }
+
+// ()=>{
+
+// <!-- Fat arrow function -->
+
+// console.log("Hello")
+// }
+
+// a =>{
+
+//   <!-- Fat arrow function with single argument -->
+
+// }
+
+// <!-- ()=>12   -> Fat arrow function that returns something -->
+
+// # Async in Js : -
+
+// -
+
+// # Callbacks , Promises and async and await in Js :-
+
+// - These all are used to run the code asynchronously...
+
+// # How internet works :-
+
+// - Internet move to the nearest tower form our system in the form of the radio wave and the tower convert these radio wave in the electrical form and transfer the data to the nearest ISP- (Internet service provider) and then it transfer the request to server and the server will serve the request..
+// - This kind of the network is term as internet.
+
+// # IP Address : two types
+
+// 1. IPv4 - Small When this model creaed there are very less devices that why it is small.
+// 2. IPv6 - Large - Larges of devices being created that is why is big.
+
+// - It is provided by the ISP to our system . Whenever we connect our system from the internet.
+// - Provided by a protocol name as Dynamic Host Configuration Protocol (DHCP).
+
+// # how node js Works :-
+
+// - Node js is neither a library nor a framework it is javascript run time enviroment that provided some dependency to run js on the server side..
+
+// - Non blocking i/o :- First code will never stops second code from execution..
+
+// - Asynchronous is the functionality through which the non-blocking nature can be achieved..
+
+// # libuv :-
+
+// - Libuv is a multi-platform, open-source library that provides support for asynchronous I/O operations. Initially designed for Node.js, it is now used by various software projects. It is designed around an event-driven, asynchronous I/O model, which allows Node.js to handle many concurrent operations efficiently.
+
+// Node js is Good when it comes to serving a lots of request and connect to many client but not good at when comes to CPU intensive tasks that requires a lots of CPU computation..
+
+
