dada8bad0282521ae1627598338f193c07a79b7c
Mayankkumar1234 <mk1070355@gmail.com>
2025-05-30 21:33:33 +0530

    Practise

---
+++ CodingQuestions/Q3.js
@@ -0,0 +1,11 @@
+
+// age = 100;
+
+// console.log("Value of age is :", age);  // print refrence error : Cannot read the value of x because due to let keyword because at the time age is in temporal dead zone.
+
+// let age =21; 
+
+
+age =21;
+
+console.log()
